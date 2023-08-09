0b902d79a27c7cb3cbeab82a4b478c9b9b796414
kendyn <kendynweb@gmail.com>
2023-08-09 12:38:12 -0400

    deleted duplicate code blocks

--- js/main.js
+++ js/main.js
@@ -92,43 +92,6 @@ function generateTile(index, color) {
     console.log('This is the tile color',color);
     return tile
 }
-//Match Logic
-
-function checkColorMatch (tile1, tile2, tiles) {
-    const color1 = tile1.style.backgroundColor
-    const color2 = tile2.style.backgroundColor
-
-    console.log(`Checking color match: ${color1} and ${color2}`)
-
-    //match found
-    if (color1 === color2) {
-        console.log('matched')
-    } else {
-        console.log('No match')
-        // Delay before resetting colors
-        setTimeout(() => {
-            //reset colors with delay
-            tile1.style.backgroundColor = tile1.dataset.originalColor
-            tile2.style.backgroundColor = tile2.dataset.originalColor
-        }, 1000); 
-    }
-}
-
-
-
-//random color for flipped tile
-function setRandomBrightColor(){
-    const brightColors =  ['#007BFF', '#FF0000', '#28A745', '#FFC107', '#6F42C1', '#FD7E14']
-    const randomColorIndex = Math.floor(Math.random() * brightColors.length)
-    return brightColors[randomColorIndex]
-}
-
-//random Gray color for unflipped tile
-function setRandomGrayColor(){
-    const grayColors = ['#A9A9A9', '#808080', '#696969', '#778899', '#708090', '#2F4F4F', '#F0F0F0', '#D0D0D0', '#B0B0B0', '#909090', '#707070', '#505050']
-    const randomColorIndex = Math.floor(Math.random() * grayColors.length)
-    return grayColors[randomColorIndex]
-} 
 
 //Match Logic
 
@@ -147,9 +110,12 @@ function checkColorMatch (tile1, tile2, tiles) {
         // Delay before resetting colors
         setTimeout(() => {
             //reset colors with delay
-            
             tile1.style.backgroundColor = tile1.dataset.originalColor
             tile2.style.backgroundColor = tile2.dataset.originalColor
+        
+            //Reset flipped state
+            tile1.dataset.flipped = 'false'
+            tile2.dataset.flipped = 'false'
         }, 1000); 
     }
 }
@@ -162,6 +128,11 @@ document.addEventListener('DOMContentLoaded', () => {
     //click event fo every ttile generated
     tiles.forEach((tile) => {
         tile.addEventListener('click', () => {
+            
+            if (tile.classList.contains('matched')) {
+                return
+            }
+           
             if (selectedTiles.length < 2) {
                 selectedTiles.push(tile)
                 tile.style.border = '5px solid #006767'
@@ -174,31 +145,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     });
                     selectedTiles = []
         }
-
-        })
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    const tiles = document.querySelectorAll('.tile')
-    //store user selection
-    let selectedTiles =[]
-    
-    //click event fo every ttile generated
-    tiles.forEach((tile) => {
-        tile.addEventListener('click', () => {
-            if (selectedTiles.length < 2) {
-                selectedTiles.push(tile)
-                tile.style.border = '5px solid #006767'
-            }
-    
-            if (selectedTiles.length === 2) {
-                checkColorMatch(selectedTiles[0], selectedTiles[1], tiles)
-                selectedTiles.forEach((selectedTile) => {
-                    selectedTile.style.border = '5px solid #fff'
-                });
-                selectedTiles = []
-            }
 
         })
     })
